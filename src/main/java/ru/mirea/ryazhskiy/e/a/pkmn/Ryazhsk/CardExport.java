983b479eb80013ecc7b4038577a1273f0033c39f
Egor <kamerun2005kvg@gmail.com>
2024-11-07 17:02:18 +0300

    Upd 2.0

--- src/main/java/ru/mirea/ryazhskiy/e/a/pkmn/Ryazhsk/CardExport.java
+++ src/main/java/ru/mirea/ryazhskiy/e/a/pkmn/Ryazhsk/CardExport.java
@@ -1,5 +1,6 @@
 package ru.mirea.ryazhskiy.e.a.pkmn.Ryazhsk;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -8,14 +9,17 @@ import ru.mirea.ryazhskiy.e.a.pkmn.*;
 public class CardExport {
     public static void cardSerialization(Card target) {
         try {
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("src\\main\\resources\\crd\\" + target.getName() + ".crd"));
-
+            File dir = new File("src/main/resources/crd");
+            if (!dir.exists()) {
+                dir.mkdirs(); // Создаём директорию, если она не существует
+            }
+            // Пишем файл в указанную директорию
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(new File(dir, target.getName() + ".crd")));
             out.writeObject(target);
             out.flush();
             out.close();
-        }
-        catch (IOException e){
-            throw new RuntimeException("Ошибка создания файла");
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка создания файла: " + e.getMessage());
         }
     }
 }
--- src/main/java/ru/mirea/ryazhskiy/e/a/pkmn/Ryazhsk/PkmnApplication.java
+++ src/main/java/ru/mirea/ryazhskiy/e/a/pkmn/Ryazhsk/PkmnApplication.java
@@ -1,23 +1,72 @@
-package Ryazhsk;
-import com.fasterxml.jackson.databind.JsonNode;
+package ru.mirea.ryazhskiy.e.a.pkmn.Ryazhsk;
+
 import ru.mirea.ryazhskiy.e.a.pkmn.Card;
-import ru.mirea.ryazhskiy.e.a.pkmn.Student;
-import ru.mirea.ryazhskiy.e.a.pkmn.web.http.*;
-import ru.mirea.ryazhskiy.e.a.pkmn.web.jdbc.DatabaseService;
+import ru.mirea.ryazhskiy.e.a.pkmn.web.http.PkmnHttpClient;
 import ru.mirea.ryazhskiy.e.a.pkmn.web.jdbc.DatabaseServiceImpl;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.stream.Collectors;
 
 public class PkmnApplication {
     public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException {
+        // Создаем HTTP-клиент и сервис базы данных
         PkmnHttpClient httpClient = new PkmnHttpClient();
         DatabaseServiceImpl db = new DatabaseServiceImpl();
 
+        // Импортируем данные карточки из текстового файла
         Card myCard = CardImport.importCardFromTxt("src/main/resources/my_card.txt");
+        // Обновляем атакующие навыки карточки с использованием HTTP-клиента
         CardImport.updateAttackSkill(myCard, httpClient);
 
+        // Сохраняем карточку в базе данных
         db.saveCardToDatabase(myCard);
+
+        // Выводим характеристики карточки на экран
+        System.out.println("Характеристики карты:");
+        System.out.println("Стадия покемона: " + myCard.getPokemonStage());
+        System.out.println("Имя: " + myCard.getName());
+        System.out.println("HP: " + myCard.getHp());
+        System.out.println("Тип покемона: " + myCard.getPokemonType());
+        System.out.println("Эволюция из: " + (myCard.getEvolvesFrom() != null ? myCard.getEvolvesFrom().getName() : "None"));
+        System.out.println("Список умений:");
+        myCard.getSkills().forEach(skill -> {
+            System.out.println("  Умение: " + skill.getName());
+            System.out.println("    Описание: " + skill.getDescription());
+            System.out.println("    Стоимость: " + skill.getCost());
+            System.out.println("    Урон: " + skill.getDamage());
+        });
+        System.out.println("Тип слабости: " + (myCard.getWeaknessType() != null ? myCard.getWeaknessType() : "None"));
+        System.out.println("Тип сопротивления: " + (myCard.getResistanceType() != null ? myCard.getResistanceType() : "None"));
+        System.out.println("Стоимость отступления: " + myCard.getRetreatCost());
+        System.out.println("Игровой набор: " + myCard.getGameSet());
+        System.out.println("Знак регуляции: " + myCard.getRegulationMark());
+        System.out.println("Владелец карты: " + myCard.getPokemonOwner());
+        System.out.println("Номер: " + myCard.getNumber());
+
+        // Создаём и используем экземпляр CardExport для сохранения карточки на диск
+        CardExport.cardSerialization(myCard);
+        System.out.println("Карточка успешно сохранена на диск.");
+
+       //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        // Десериализация объекта Flygon из файла Flygon.crd
+        /*Card importCard = CardImport.cardImportByte("src/main/resources/crd/Flygon.crd");
+        System.out.println("\nЗагружена карта покемона:");
+        System.out.println("Имя: " + importCard.getName());
+        System.out.println("HP: " + importCard.getHp());
+        System.out.println("Тип покемона: " + importCard.getPokemonType());
+        System.out.println("Список умений:");
+        importCard.getSkills().forEach(skill -> {
+            System.out.println("  Умение: " + skill.getName());
+            System.out.println("    Описание: " + skill.getDescription());
+            System.out.println("    Стоимость: " + skill.getCost());
+            System.out.println("    Урон: " + skill.getDamage());
+        });
+        System.out.println("Тип слабости: " + (importCard.getWeaknessType() != null ? importCard.getWeaknessType() : "None"));
+        System.out.println("Стоимость отступления: " + importCard.getRetreatCost());
+        System.out.println("Игровой набор: " + importCard.getGameSet());
+        System.out.println("Знак регуляции: " + importCard.getRegulationMark());
+        System.out.println("Владелец карты " + importCard.getPokemonOwner());
+        System.out.println("Номер: " + importCard.getNumber());*/
     }
 }
